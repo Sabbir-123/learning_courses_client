3d7913cafa312ac44859061068553adcb6e39f1b
Md Sabbir Ahmed <mdsabbirahmed@Mds-MacBook-Air.local>
2022-10-26 03:47:34 +0600

    committed again

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import jsPDF from "jspdf";
+
 
 const CourseDetails = () => {
   const language = useLoaderData();
   const { _id, title, image_url, details } = language;
+   const generatePDF= ()=>{
+        const doc = new jsPDF("p", "pt", "a4")
+        doc.html(document.querySelector("#content"), 
+        {
+            callback: function(pdf){
+                pdf.save("mypdf.pdf")
+            }
+        }
+        )
+  }
   return (
     <div className="grid justify-center dark:bg-gray-700">
-      <div className="max-w-xl p-10 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
+      <div className="max-w-xl p-10 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50" id="content">
         <div className="mt-6 mb-2">
-          <ReactToPdf></ReactToPdf>
+          <button onClick={generatePDF} type="primary" className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-indigo-900 hover:bg-black focus:shadow-outline focus:outline-none">
+            React to PDF
+          </button>
         </div>
-        <div className="mt-6 mb-2">
+        <div className="mt-6 mb-2" >
           <h2 className="text-xl font-semibold tracking-wide">{title}</h2>
         </div>
         <img
